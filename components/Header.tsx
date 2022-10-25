035fd40295e2f54b615ddb80ab8a73a14aa299f9
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-10-25 14:22:12 +0100

    feat: update wallet connect implementation

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,20 +4,46 @@ import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { FC, useEffect, useState } from 'react'
-
 import {
   fromLeftAnimation,
   fromRightAnimation,
   fromTopAnimation,
 } from '../utils/animations'
+import useWalletConnect from '../utils/hooks/useWalletConnect'
 import useWindowDimensions from '../utils/hooks/useWindowDimensions'
 
 const ConnectButton: FC = () => {
-  const { active, account } = useWeb3React<Web3Provider>()
   const router = useRouter()
+  // const injectedConnector = new InjectedConnector({
+  //   supportedChainIds: [1, 3, 4, 5, 42],
+  // })
+  const { active, account } = useWeb3React<Web3Provider>()
+  const handleConnect = useWalletConnect()
+  const [isHovered, setIsHovered] = useState(false)
+
+  // const handleConnect = async (active?: boolean) => {
+  //   if (!window.ethereum) {
+  //     console.log('Please Install Metamask')
+  //     return
+  //   }
+  //   if (!active) {
+  //     try {
+  //       await activate(injectedConnector)
+  //       router.push('/collections')
+  //     } catch (e) {
+  //       console.error(e)
+  //     }
+  //   } else {
+  //     try {
+  //       deactivate()
+  //     } catch (e) {
+  //       console.error(e)
+  //     }
+  //   }
+  // }
 
   const handleClick = () => {
-    router.push('/connect-wallet')
+    handleConnect('/collections')
   }
 
   return (
@@ -32,12 +58,16 @@ const ConnectButton: FC = () => {
         duration: 0.2,
         delay: 1.4,
       }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      {!active ? (
+      {!active && router.asPath === '/' ? (
+        'Join Community'
+      ) : !active ? (
         'Connect Wallet'
       ) : (
         <span>
-          Connected{' '}
+          {isHovered ? 'Disconnect' : 'Connected'}{' '}
           <span className="lg:text-xs">
             {account?.substring(0, 4)}...
             {account?.substring(account.length - 2)}
@@ -48,10 +78,7 @@ const ConnectButton: FC = () => {
   )
 }
 
-const Header: FC = () => {
-  const router = useRouter()
-  const { width } = useWindowDimensions()
-  const [clientWidth, setClientWidth] = useState(0)
+const Search: FC = () => {
   const [isClicked, setIsClicked] = useState(false)
   const [searchWidth, setSearchWidth] = useState('')
 
@@ -60,89 +87,111 @@ const Header: FC = () => {
     else setSearchWidth('w-[33px]')
   }, [isClicked])
 
+  return (
+    <>
+      {/* DESKTOP */}
+      <motion.div
+        className="hidden md:block relative h-9 md:w-56"
+        variants={fromTopAnimation}
+        initial="initial"
+        animate="final"
+        transition={{
+          ease: 'easeInOut',
+          duration: 0.4,
+          delay: 1,
+        }}
+      >
+        <span className="absolute left-1 top-2 w-fit z-20 text-primary font-bold md:top-3">
+          <Image
+            src="/images/icons/search.svg"
+            alt="search"
+            width="25px"
+            height="15px"
+          />
+        </span>
+        <input
+          type="text"
+          className="w-full h-full px-7 rounded caret-custom_yellow placeholder:text-custom_yellow font-light text-white 
+      focus:border focus:border-custom_yellow focus:outline-none bg-custom_grey md:h-11"
+        />
+        <span className="absolute right-3 top-2 w-fit z-20 text-[#898989] font-light text-sm cursor-pointer md:top-3">
+          X
+        </span>
+      </motion.div>
+      {/* MOBILE */}
+      <div
+        className={`bg-custom_grey h-[33px] md:hidden ${
+          !isClicked && 'p-1'
+        } transition-all duration-500 flex 
+                justify-end items-center gap-1 ${searchWidth}
+                ${!isClicked ? 'rounded-full' : 'rounded-md'}`}
+      >
+        <Image
+          src="/images/icons/search.svg"
+          alt="search"
+          width="25px"
+          height="15px"
+          onClick={() => setIsClicked((prev) => !prev)}
+        />
+        <input
+          type="text"
+          className={`${
+            isClicked ? 'w-full' : 'hidden'
+          } h-full pl-1 rounded caret-custom_yellow placeholder:text-custom_yellow text-[#898989] font-light
+                focus:border focus:border-custom_yellow focus:outline-none bg-custom_grey md:h-11`}
+        />
+      </div>
+    </>
+  )
+}
+
+const Logo: FC = () => {
+  const router = useRouter()
+  const { width } = useWindowDimensions()
+  const [clientWidth, setClientWidth] = useState(0)
+
   useEffect(() => {
     setClientWidth(width)
   }, [width])
 
   return (
-    <nav className="p-4 pt-6 lg:px-16">
-      <div className="grid grid-cols-12 gap-1">
-        <motion.div
-          className="col-span-3 md:col-span-6 "
-          variants={fromLeftAnimation}
-          initial="initial"
-          animate="final"
-          transition={{
-            ease: 'easeInOut',
-            duration: 0.6,
-            delay: 0.4,
-          }}
-        >
-          <Image
-            src="/images/icons/logo.svg"
-            alt="nftzone_logo"
-            width={clientWidth > 768 ? '188px' : '100px'}
-            height={clientWidth > 768 ? '64px' : '46px'}
-            className="cursor-pointer"
-            onClick={() => router.push('/')}
-          />
-        </motion.div>
-        <div className="col-span-9 md:col-span-6 flex justify-end gap-2 md:gap-6 lg:gap-16">
-          <motion.div
-            className="hidden md:block relative h-9 md:w-56"
-            variants={fromTopAnimation}
-            initial="initial"
-            animate="final"
-            transition={{
-              ease: 'easeInOut',
-              duration: 0.4,
-              delay: 1,
-            }}
-          >
-            <span className="absolute left-1 top-2 w-fit z-20 text-primary font-bold md:top-3">
-              <Image
-                src="/images/icons/search.svg"
-                alt="search"
-                width="25px"
-                height="15px"
-              />
-            </span>
-            <input
-              type="text"
-              className="w-full h-full px-7 rounded caret-custom_yellow placeholder:text-custom_yellow font-light text-white 
-              focus:border focus:border-custom_yellow focus:outline-none bg-custom_grey md:h-11"
-            />
-            <span className="absolute right-3 top-2 w-fit z-20 text-[#898989] font-light text-sm cursor-pointer md:top-3">
-              X
-            </span>
-          </motion.div>
+    <motion.div
+      variants={fromLeftAnimation}
+      initial="initial"
+      animate="final"
+      transition={{
+        ease: 'easeInOut',
+        duration: 0.6,
+        delay: 0.4,
+      }}
+    >
+      <Image
+        src="/images/icons/logo.svg"
+        alt="nftzone_logo"
+        width={clientWidth > 768 ? '188px' : '100px'}
+        height={clientWidth > 768 ? '64px' : '46px'}
+        className="cursor-pointer"
+        onClick={() => router.push('/')}
+      />
+    </motion.div>
+  )
+}
 
-          <div
-            className={`bg-custom_grey h-[33px] md:hidden ${
-              !isClicked && 'p-1'
-            } transition-all duration-500 flex 
-              justify-end items-center gap-1 ${searchWidth}
-              ${!isClicked ? 'rounded-full' : 'rounded-md'}`}
-          >
-            <Image
-              src="/images/icons/search.svg"
-              alt="search"
-              width="25px"
-              height="15px"
-              onClick={() => setIsClicked((prev) => !prev)}
-            />
-            <input
-              type="text"
-              className={`${
-                isClicked ? 'w-full' : 'hidden'
-              } h-full pl-1 rounded caret-custom_yellow placeholder:text-custom_yellow text-[#898989] font-light
-              focus:border focus:border-custom_yellow focus:outline-none bg-custom_grey md:h-11`}
-            />
-          </div>
+const Header: FC = () => {
+  const router = useRouter()
+
+  return (
+    <header className="p-4 pt-6 lg:px-16">
+      <div className="grid grid-cols-12 gap-1">
+        <div className="col-span-3 md:col-span-6">
+          <Logo />
+        </div>
+        <div className="col-span-9 md:col-span-6 flex justify-end gap-2 md:gap-6 lg:gap-16">
+          {router.asPath !== '/' && <Search />}
           <ConnectButton />
         </div>
       </div>
-    </nav>
+    </header>
   )
 }
 
---
+++ utils/hooks/useWalletConnect.ts
@@ -0,0 +1,35 @@
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { useRouter } from 'next/router'
+
+export default function useWalletConnect() {
+  const router = useRouter()
+  const injectedConnector = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42],
+  })
+  const { activate, deactivate, active } = useWeb3React<Web3Provider>()
+
+  const handleConnect = async (route?: string) => {
+    if (!window.ethereum) {
+      console.log('Please Install Metamask')
+      return
+    }
+    if (!active) {
+      try {
+        await activate(injectedConnector)
+        route && router.push(route)
+      } catch (e) {
+        console.error(e)
+      }
+    } else {
+      try {
+        deactivate()
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+
+  return handleConnect
+}
