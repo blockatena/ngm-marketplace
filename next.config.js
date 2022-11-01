3b7925cb32d31173aa5409732b962f8aff07084b
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-11-01 14:34:26 +0100

    build: update next config

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'nftstorage.link',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig
