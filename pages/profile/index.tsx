9be1996aae4a219a10b20dc990f528f1f4d32c9f
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-12-03 13:26:07 +0100

    feat: google authentication

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,9 +1,11 @@
+import { useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios'
 import { AnimatePresence, motion } from 'framer-motion'
 import { NextPage } from 'next'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
 import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
 import { FaHamburger } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 import { useAccount, useMutation } from 'wagmi'
 import AvatarCard from '../../components/AvatarCard'
 import Pagination from '../../components/Pagination'
@@ -25,7 +27,17 @@ import {
 } from '../../utils/animations'
 import useWindowDimensions from '../../utils/hooks/useWindowDimensions'
 
-const routes = [
+type RouteNameType =
+  | 'category'
+  | 'messages'
+  | 'my collection'
+  | 'wallet'
+  | 'history'
+  | 'settings'
+
+type RouteType = { name: RouteNameType; route: string; icon: any }
+
+const routes: RouteType[] = [
   { name: 'category', route: '/', icon: categoryIcon },
   { name: 'messages', route: '/', icon: messageIcon },
   { name: 'my collection', route: '/', icon: collectionIcon },
@@ -34,146 +46,24 @@ const routes = [
   { name: 'settings', route: '/', icon: settingsIcon },
 ]
 
-// const avatars: AvatarType[] = [
-//   {
-//     _id: '',
-//     contract_address: '',
-//     contract_type: '',
-//     token_id: '0',
-//     meta_data_url: '',
-//     is_in_auction: false,
-//     is_in_sale: false,
-//     token_owner: '',
-//     createdAt: '',
-//     updatedAt: '',
-//     __v: 0,
-//     meta_data: {
-//       name: '',
-//       image: '',
-//       description: '',
-//       external_uri: '',
-//       attributes: [{ name: '', value: '' }],
-//     },
-//   },
-//   {
-//     _id: '',
-//     contract_address: '',
-//     contract_type: '',
-//     token_id: '0',
-//     meta_data_url: '',
-//     is_in_auction: false,
-//     is_in_sale: false,
-//     token_owner: '',
-//     createdAt: '',
-//     updatedAt: '',
-//     __v: 0,
-//     meta_data: {
-//       name: '',
-//       image: '',
-//       description: '',
-//       external_uri: '',
-//       attributes: [{ name: '', value: '' }],
-//     },
-//   },
-//   {
-//     _id: '',
-//     contract_address: '',
-//     contract_type: '',
-//     token_id: '0',
-//     meta_data_url: '',
-//     is_in_auction: false,
-//     is_in_sale: false,
-//     token_owner: '',
-//     createdAt: '',
-//     updatedAt: '',
-//     __v: 0,
-//     meta_data: {
-//       name: '',
-//       image: '',
-//       description: '',
-//       external_uri: '',
-//       attributes: [{ name: '', value: '' }],
-//     },
-//   },
-//   // {
-//   //   contract_details: {
-//   //     _id: '',
-//   //     symbol: '',
-//   //     owner_address: '',
-//   //     collection_name: '',
-//   //     chain: '',
-//   //     type: '',
-//   //     transactionhash: '',
-//   //     contract_address: '',
-//   //     description: '',
-//   //     baseuri: '',
-//   //     imageuri: ['', ''],
-//   //     createdAt: '',
-//   //     updatedAt: '',
-//   //     __v: 0,
-//   //   },
-//   //   nft: {
-//   //     _id: '',
-//   //     contract_address: '',
-//   //     contract_type: '',
-//   //     token_id: '0',
-//   //     meta_data_url: '',
-//   //     is_in_auction: false,
-//   //     is_in_sale: false,
-//   //     token_owner: '',
-//   //     createdAt: '',
-//   //     updatedAt: '',
-//   //     __v: 0,
-//   //   },
-//   // },
-//   // {
-//   //   token_id: '12',
-//   //   name: 'Crypto',
-//   //   img: '/images/auction/auction_img_6.svg',
-//   //   is_in_auction: false,
-//   //   contract_address: '0xfd2b3561630c02b8047B911c22d3f3bfF3ad64Ce',
-//   //   contract_type: '',
-//   //   createdAt: '',
-//   //   is_in_sale: false,
-//   //   meta_data_url: '',
-//   //   token_owner: '',
-//   //   updatedAt: '',
-//   //   __v: 0,
-//   //   _id: '',
-//   //   contract_details: {
-//   //     _id: '',
-//   //     symbol: '',
-//   //     owner_address: '',
-//   //     collection_name: '',
-//   //     chain: '',
-//   //     type: '',
-//   //     transactionhash: '',
-//   //     contract_address: '',
-//   //     baseuri: '',
-//   //     createdAt: '',
-//   //     updatedAt: '',
-//   //     __v: 0,
-//   //     description: '',
-//   //     imageuri: ['', ''],
-//   //   },
-//   // },
-// ]
-
 const NavRoute: FC<{
   icon: string
-  name: string
+  name: RouteNameType
   route: string
   isDrawer?: boolean
-}> = ({ icon, name, route, isDrawer }) => {
-  const router = useRouter()
+  setCurrentRoute: Dispatch<SetStateAction<RouteNameType>>
+  currentRoute: RouteNameType
+}> = ({ icon, name, setCurrentRoute, currentRoute }) => {
   const handleClick = () => {
-    if (isDrawer) return
-    router.push(route)
+    // if (isDrawer) return
+    setCurrentRoute(name)
   }
 
   return (
     <div
-      className="p-1 flex gap-8 cursor-pointer my-5 hover:bg-[#41391C] text-white hover:text-[#FFC400]"
+      className={`p-1 flex gap-8 cursor-pointer my-5 ${
+        currentRoute === name && 'bg-[#41391C]'
+      } hover:bg-[#41391C] text-white hover:text-[#FFC400] rounded-md`}
       onClick={handleClick}
     >
       <Image src={icon} alt="icon" className="w-2" />
@@ -185,9 +75,10 @@ const NavRoute: FC<{
 const Drawer: FC<{
   isOpen: boolean
   setIsOpen: Dispatch<SetStateAction<boolean>>
-}> = ({ setIsOpen }) => {
+  setCurrentRoute: Dispatch<SetStateAction<RouteNameType>>
+  currentRoute: RouteNameType
+}> = ({ setIsOpen, setCurrentRoute, currentRoute }) => {
   const handleClick = () => {
-    //route to new route
     setIsOpen(false)
   }
 
@@ -200,32 +91,38 @@ const Drawer: FC<{
       </div>
       {routes?.map(({ icon, name, route }) => (
         <div key={name} onClick={handleClick}>
-          <NavRoute icon={icon} name={name} route={route} isDrawer={true} />
+          <NavRoute
+            icon={icon}
+            name={name}
+            route={route}
+            isDrawer={true}
+            setCurrentRoute={setCurrentRoute}
+            currentRoute={currentRoute}
+          />
         </div>
       ))}
     </div>
   )
 }
 
-const ITEMS_PER_PAGE = 12
-const ALPHABETICAL_ORDER = 'AtoZ'
-const ORDER = 'NewToOld'
+const UserActivity = () => {
+  return (
+    <div
+      className="pb-20 md:px-4 bg-[#1F2021] rounded-lg grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
+gap-20 w-full  max-w-full mx-auto px-6 py-9 lg:h-[928px] scrollbar-thin scrollbar-thumb-[#5A5B61] scrollbar-thumb-rounded-lg scrollbar-track-[#1F2021] overflow-y-scroll"
+    >
+      <h2 className="font-poppins text-white text-center">User Activity</h2>
+    </div>
+  )
+}
 
-const ProfilePage: NextPage = () => {
-  const [isCollections, setIsCollections] = useState(true)
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+const UserAssets = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(1)
   const [nfts, setNfts] = useState<AvatarType[]>()
   const { width } = useWindowDimensions()
   const { address } = useAccount()
   const { mutate, data } = useMutation(getCollectionNfts)
-
-  // const { data } = useQuery(
-  //   [QUERIES.getUserNfts, address],
-  //   () => getUserNfts(address || '', currentPage),
-  //   { enabled: !!address }
-  // )
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
 
@@ -265,6 +162,136 @@ const ProfilePage: NextPage = () => {
   }
 
   return (
+    <>
+      <div
+        className="pb-20 md:px-4 bg-[#1F2021] rounded-lg grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
+gap-20 w-full  max-w-full mx-auto px-6 py-9 lg:h-[928px] scrollbar-thin scrollbar-thumb-[#5A5B61] scrollbar-thumb-rounded-lg scrollbar-track-[#1F2021] overflow-y-scroll"
+      >
+        {nfts?.length &&
+          nfts.map((cardData, index) => (
+            <motion.div
+              className="flex justify-center"
+              key={index}
+              variants={opacityAnimation}
+              initial="initial"
+              whileInView="final"
+              viewport={{ once: true }}
+              transition={{
+                ease: 'easeInOut',
+                duration: 0.6,
+                delay: handleDelay(index),
+              }}
+            >
+              <AvatarCard {...cardData} />
+            </motion.div>
+          ))}
+        {nfts?.length === 0 && (
+          <p className="text-white font-poppins p-4">
+            No NFTs owned by this user
+          </p>
+        )}
+      </div>
+      <div className="flex justify-end p-4 pb-10">
+        <Pagination
+          totalPages={totalPages}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </div>
+    </>
+  )
+}
+
+const AssetsActivityTabs: FC<{
+  setIsDrawerOpen: Dispatch<SetStateAction<boolean>>
+}> = ({ setIsDrawerOpen }) => {
+  const [currentTab, setCurrentTab] = useState<'assets' | 'activity'>('assets')
+
+  return (
+    <>
+      <div className="text-white font-poppins text-[20px] pl-[25%] p-6">
+        <span
+          className="border-b-2 border-custom_yellow cursor-pointer transition-all"
+          style={currentTab !== 'assets' ? { border: 'none' } : {}}
+          onClick={() => setCurrentTab('assets')}
+        >
+          Assets
+        </span>{' '}
+        <span className="text-gray-500">|</span>{' '}
+        <span
+          className="border-b-2 border-custom_yellow cursor-pointer transition-all"
+          style={currentTab !== 'activity' ? { border: 'none' } : {}}
+          onClick={() => setCurrentTab('activity')}
+        >
+          Activity
+        </span>
+      </div>
+      <div
+        onClick={() => setIsDrawerOpen(true)}
+        className="text-white p-4 lg:hidden"
+      >
+        <FaHamburger className=" text-lg hover:text-custom_yellow text-[#E5E5E5]" />
+      </div>
+      {currentTab === 'activity' ? <UserActivity /> : <UserAssets />}
+    </>
+  )
+}
+
+const UserSettings = () => {
+  const handleLogin = useGoogleLogin({
+    onSuccess: async (respose) => {
+      try {
+        const res = await axios.get(
+          'https://www.googleapis.com/oauth2/v3/userinfo',
+          {
+            headers: {
+              Authorization: `Bearer ${respose.access_token}`,
+            },
+          }
+        )
+        console.log('USER', res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  })
+  return (
+    <>
+      <div className="text-white font-poppins text-[20px] pl-[25%] p-6">
+        <span className="border-b-2 border-custom_yellow">Settings</span>
+      </div>
+      <div
+        // onClick={() => setIsDrawerOpen(true)}
+        className="text-white p-4 lg:hidden"
+      >
+        <FaHamburger className=" text-lg hover:text-custom_yellow text-[#E5E5E5]" />
+      </div>
+      <div
+        className="pb-20 md:px-4 bg-[#1F2021] rounded-lg grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
+gap-20 w-full  max-w-full mx-auto px-6 py-9 lg:h-[928px] scrollbar-thin scrollbar-thumb-[#5A5B61] scrollbar-thumb-rounded-lg scrollbar-track-[#1F2021] overflow-y-scroll"
+      >
+        <div>
+          <button
+            onClick={() => handleLogin()}
+            className="btn-primary p-2 rounded-lg flex items-center gap-1"
+          >
+            <FcGoogle /> Sign In with Google
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const ITEMS_PER_PAGE = 12
+const ALPHABETICAL_ORDER = 'AtoZ'
+const ORDER = 'NewToOld'
+
+const ProfilePage: NextPage = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [currentRoute, setCurrentRoute] = useState<RouteNameType>('category')
+
+  return (
     <main className="p-4 pt-6 pb-0 lg:px-8 relative -bottom-8">
       <div
         className="w-full h-[351px] bg-cover rounded-t-lg relative flex justify-center mb-40"
@@ -293,10 +320,10 @@ const ProfilePage: NextPage = () => {
           </p>
         </motion.div>
       </div>
-      <div className="grid place-items-center">
-        <div className="grid grid-cols-12 gap-4 w-max">
+      <div className="grid place-items-center w-full">
+        <div className="grid grid-cols-12 gap-4 w-full">
           <motion.div
-            className="hidden lg:block lg:col-span-3 h-[928px] p-10 bg-[#1F2021] rounded-lg"
+            className="hidden lg:block lg:col-span-3 h-[928px] p-10 bg-[#1F2021]  rounded-lg"
             variants={fromLeftAnimation}
             initial="initial"
             whileInView="final"
@@ -308,7 +335,14 @@ const ProfilePage: NextPage = () => {
             }}
           >
             {routes?.map(({ icon, name, route }) => (
-              <NavRoute key={name} icon={icon} name={name} route={route} />
+              <NavRoute
+                key={name}
+                icon={icon}
+                name={name}
+                route={route}
+                setCurrentRoute={setCurrentRoute}
+                currentRoute={currentRoute}
+              />
             ))}
           </motion.div>
           <motion.div
@@ -323,64 +357,10 @@ const ProfilePage: NextPage = () => {
               delay: 0.8,
             }}
           >
-            <div className="text-white font-poppins text-[20px] pl-[25%] p-6">
-              <span
-                className="border-b-2 border-custom_yellow cursor-pointer transition-all"
-                style={!isCollections ? { border: 'none' } : {}}
-                onClick={() => setIsCollections(true)}
-              >
-                Collections
-              </span>{' '}
-              <span className="text-gray-500">|</span>{' '}
-              <span
-                className="border-b-2 border-custom_yellow cursor-pointer transition-all"
-                style={isCollections ? { border: 'none' } : {}}
-                onClick={() => setIsCollections(false)}
-              >
-                Activity
-              </span>
-            </div>
-            <div
-              onClick={() => setIsDrawerOpen(true)}
-              className="text-white p-4 lg:hidden"
-            >
-              <FaHamburger className=" text-lg hover:text-custom_yellow text-[#E5E5E5]" />
-            </div>
-            <div
-              className="pb-20 md:px-4 bg-[#1F2021] rounded-lg grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
-          gap-20 w-full  max-w-full mx-auto px-6 py-9 lg:h-[928px] scrollbar-thin scrollbar-thumb-[#5A5B61] scrollbar-thumb-rounded-lg scrollbar-track-[#1F2021] overflow-y-scroll"
-            >
-              {nfts?.length &&
-                nfts.map((cardData, index) => (
-                  <motion.div
-                    className="flex justify-center"
-                    key={index}
-                    variants={opacityAnimation}
-                    initial="initial"
-                    whileInView="final"
-                    viewport={{ once: true }}
-                    transition={{
-                      ease: 'easeInOut',
-                      duration: 0.6,
-                      delay: handleDelay(index),
-                    }}
-                  >
-                    <AvatarCard {...cardData} />
-                  </motion.div>
-                ))}
-              {nfts?.length === 0 && (
-                <p className="text-white font-poppins p-4">
-                  No NFTs owned by this user
-                </p>
-              )}
-            </div>
-            <div className="flex justify-end p-4 pb-10">
-              <Pagination
-                totalPages={totalPages}
-                paginate={paginate}
-                currentPage={currentPage}
-              />
-            </div>
+            {currentRoute === 'category' && (
+              <AssetsActivityTabs setIsDrawerOpen={setIsDrawerOpen} />
+            )}
+            {currentRoute === 'settings' && <UserSettings />}
           </motion.div>
         </div>
       </div>
@@ -397,7 +377,12 @@ const ProfilePage: NextPage = () => {
               duration: 0.25,
             }}
           >
-            <Drawer isOpen={isDrawerOpen} setIsOpen={setIsDrawerOpen} />
+            <Drawer
+              isOpen={isDrawerOpen}
+              setIsOpen={setIsDrawerOpen}
+              setCurrentRoute={setCurrentRoute}
+              currentRoute={currentRoute}
+            />
           </motion.div>
         )}
       </AnimatePresence>
