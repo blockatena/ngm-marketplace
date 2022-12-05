90682ad82f927e824bc057ae0604383b7e20fe9e
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-12-05 17:25:27 +0100

    fix: eplore section

--- components/sections/ExploreSection.tsx
+++ components/sections/ExploreSection.tsx
@@ -1,94 +1,138 @@
 import { motion } from 'framer-motion'
 import { useRouter } from 'next/router'
+import { FC, useCallback, useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
 import { AvatarType } from '../../interfaces'
+import { QUERIES } from '../../react-query/constants'
+import { getAllCollectionNfts } from '../../react-query/queries'
 import { opacityAnimation } from '../../utils/animations'
 import AvatarCard from '../AvatarCard'
-import { FC } from 'react'
-import { useState, useEffect } from 'react'
-const baseURL = process.env.NEXT_PUBLIC_API_URL || ''
+// const baseURL = process.env.NEXT_PUBLIC_API_URL || ''
+
+const NUMBER_TO_DISPLAY = 3
+
 const ExploreSection: FC<{
   contractAddress: string
-  tokenId:string
-}> = ({ contractAddress, tokenId}) => {
+  tokenId: string
+}> = ({ contractAddress, tokenId }) => {
   const [explore, setExplore] = useState<AvatarType[]>([])
-  const [Avatars, setAvatars] = useState<AvatarType[]>([])
-  const [token, settToken] = useState<Number>()
-  // const DATA = useQuery(
-  //   [QUERIES.getCollectionNFTs, contractAddress],
-  //   () => getCollectionNFTs(contractAddress),
-  //   { enabled: !!contractAddress }
-  // )
+  // const [Avatars, setAvatars] = useState<AvatarType[]>([])
+  // const [token, settToken] = useState<Number>()
+  const [nfts, setNfts] = useState<AvatarType[]>()
 
-  const getNFTs = ()=>{
-  if (Avatars?.length > 0){
-    return;
-  } else {
-    let url = `${baseURL}/nft/collection/${contractAddress}`
-    // console.log(url)
-    if(contractAddress){
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          if(Avatars?.length>0){
-            //
-          } else {
-          setAvatars(data.nfts)
-          }
-        });
-      }
-    }
-
-  }
-// eslint-disable-next-line react-hooks/exhaustive-deps
-useEffect(()=>{
-  if(token !== parseInt(tokenId)){
-    filter2(Avatars)
-    let tok = parseInt(tokenId)
-    settToken(tok)
-  } else
-  if(token == 0 && tokenId){
-    let tok = parseInt(tokenId)
-    settToken(tok)
-  }
-})
+  const { data } = useQuery(
+    [QUERIES.getAllCollectionNfts, contractAddress],
+    () => getAllCollectionNfts(contractAddress),
+    { enabled: !!contractAddress }
+  )
 
   useEffect(() => {
-    if (explore[2]===undefined){
-      filter2(Avatars)
-    } else if (explore?.length > 0) {
-        return
-      } else {
-        filter2(Avatars)
-      }
-  })
+    setNfts(data?.data?.nfts)
+  }, [data?.data?.nfts])
 
-  useEffect(() => {
-    if (Avatars?.length > 0) {  return}
-    else {
-      getNFTs()
-    }
-  })
+  const handleFilter = useCallback(
+    (unfilteredNfts: AvatarType[]) => {
+      if (!unfilteredNfts?.length) return
 
-  const filter2 = (Avatars: any) => {
-    if (Avatars?.length > 0) {
-      var newItems = []
-      for (var i = 0; i < 3; i++) {
-        var idx = Math.floor(Math.random() * Avatars.length)
+      const randomIndexes: number[] = []
+
+      if (unfilteredNfts.length > NUMBER_TO_DISPLAY) {
+        while (randomIndexes.length < NUMBER_TO_DISPLAY) {
+          let randomIndex = Math.floor(Math.random() * unfilteredNfts.length)
+
           if (
-            Avatars?.[idx]?.token_id !== tokenId ||
-            Avatars?.[idx]?.token_id
+            unfilteredNfts[randomIndex].token_id !== tokenId &&
+            !randomIndexes.some((index) => index === randomIndex)
           ) {
-            newItems.push(Avatars[idx])
-            Avatars.splice(idx, 1)
-          } else {i--
+            randomIndexes.push(randomIndex)
+            // setExplore((prev) => [...prev, unfilteredNfts[randomIndex]])
           }
+        }
+        const exploreData = []
+        for (let i = 0; i < NUMBER_TO_DISPLAY; i++) {
+          exploreData.push(unfilteredNfts[randomIndexes[i]])
+        }
+        setExplore(exploreData)
       }
-        setExplore(newItems)
-      
-      
-    }
-  }
 
+      if (
+        unfilteredNfts.length < NUMBER_TO_DISPLAY + 1 &&
+        randomIndexes.length < NUMBER_TO_DISPLAY
+      ) {
+        setExplore(unfilteredNfts.filter((nft) => nft.token_id !== tokenId))
+      }
+    },
+    [tokenId]
+  )
+
+  useEffect(() => {
+    nfts && handleFilter(nfts)
+  }, [nfts, handleFilter])
+
+  // const getNFTs = () => {
+  //   if (Avatars?.length > 0) {
+  //     return
+  //   } else {
+  //     let url = `${baseURL}/nft/collection/${contractAddress}`
+  //     // console.log(url)
+  //     if (contractAddress) {
+  //       fetch(url)
+  //         .then((response) => response.json())
+  //         .then((data) => {
+  //           if (Avatars?.length > 0) {
+  //             //
+  //           } else {
+  //             setAvatars(data.nfts)
+  //           }
+  //         })
+  //     }
+  //   }
+  // }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // useEffect(() => {
+  //   if (token !== parseInt(tokenId)) {
+  //     filter2(Avatars)
+  //     let tok = parseInt(tokenId)
+  //     settToken(tok)
+  //   } else if (token == 0 && tokenId) {
+  //     let tok = parseInt(tokenId)
+  //     settToken(tok)
+  //   }
+  // })
+
+  // useEffect(() => {
+  //   if (explore[2] === undefined) {
+  //     filter2(Avatars)
+  //   } else if (explore?.length > 0) {
+  //     return
+  //   } else {
+  //     filter2(Avatars)
+  //   }
+  // })
+
+  // useEffect(() => {
+  //   if (Avatars?.length > 0) {
+  //     return
+  //   } else {
+  //     getNFTs()
+  //   }
+  // })
+
+  // const filter2 = (Avatars: any) => {
+  //   if (Avatars?.length > 0) {
+  //     var newItems = []
+  //     for (var i = 0; i < 3; i++) {
+  //       var idx = Math.floor(Math.random() * Avatars.length)
+  //       if (Avatars?.[idx]?.token_id !== tokenId || Avatars?.[idx]?.token_id) {
+  //         newItems.push(Avatars[idx])
+  //         Avatars.splice(idx, 1)
+  //       } else {
+  //         i--
+  //       }
+  //     }
+  //     setExplore(newItems)
+  //   }
+  // }
 
   const router = useRouter()
   const openCollection = () => {
