f0c954dc94ac1fc499a7e92c2833434e740ceafb
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2023-02-10 10:42:40 +0100

    fix: 1155 nft sale timer

--- pages/assets/[contractAddress]/[tokenId]/index.tsx
+++ pages/assets/[contractAddress]/[tokenId]/index.tsx
@@ -229,11 +229,20 @@ const ViewAssetPage: NextPage = () => {
     },
   ]
   useEffect(() => {
+    // setEndTime(
+    //   data?.data?.auction?.end_date
+    //     ? data?.data?.auction?.end_date
+    //     : data?.data?.sale?.end_date
+    //     ? data?.data?.sale?.end_date
+    //     : ''
+    // )
     setEndTime(
       data?.data?.auction?.end_date
         ? data?.data?.auction?.end_date
         : data?.data?.sale?.end_date
         ? data?.data?.sale?.end_date
+        : data?.data?.sales?.length
+        ? data?.data?.sales[0].end_date
         : ''
     )
     setNft(data?.data?.nft || data?.data?.nft1155)
