889940376b449f5490010a8dc57043dc04b77864
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2023-02-15 12:59:59 +0100

    feat: add join community section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import GalleryAndNewsSection from '../components/sections/GalleryAndNewsSection'
 import HeroSection from '../components/sections/HeroSection'
 import HowItWorks from '../components/sections/HowItWorks'
 import HowItWorksSection from '../components/sections/HowItWorksSection'
+import JoinCommunity from '../components/sections/JoinCommunity'
 import JoinCommunitySection from '../components/sections/JoinCommunitySection'
 import LiveAuction from '../components/sections/LiveAuction'
 import LiveAuctionSection from '../components/sections/LiveAuctionSection'
@@ -23,6 +24,7 @@ const HomePage: NextPageWithLayout = () => {
         <OurCollections />
         <HowItWorks />
         <Faq />
+        <JoinCommunity />
         <div className=" w-full bg-mainHomePage bg-cover bg-no-repeat bg-center relative before:top-0 before:bottom-0 before:left-0 before:right-0 before:absolute before:bg-black before:opacity-80">
           <LiveAuctionSection />
           <GalleryAndNewsSection />
