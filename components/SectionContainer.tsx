62b600cb967419617879e5528fc2784894b25bb6
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2023-03-10 08:25:34 +0100

    style: container padding

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -4,7 +4,8 @@ import { FC, ReactNode } from 'react'
 export const OUTER_BOX_STYLE =
   'grid place-items-center w-full px-[5%] lg:px-[2%] xl:px-[6%]'
 export const INNER_BOX_STYLE =
-  'max-w-[62.5rem] xl:max-w-[120rem]  w-full lg:px-[5.93rem]  px-[.1rem]'
+  'max-w-[120rem]  w-full xl:scale-90 origin-center'
+// 'max-w-[62.5rem] xl:max-w-[120rem]  w-full lg:px-[5.93rem]  px-[.1rem] xl:scale-90 origin-center'
 
 // section container
 const SectionContainer: FC<{ children: ReactNode }> = ({ children }) => {
